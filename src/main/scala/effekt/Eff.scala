779ebfeab26ab5948019be961a19da4757a5a19e
Jonathan Brachthäuser <jonathan@b-studios.de>
2017-05-19 16:01:15 +0200

    clean up gh-pages branch

--- src/main/scala/effekt/Eff.scala
+++
@@ -1,9 +0,0 @@
-package effekt
-
-trait Eff extends Serializable {
-  type Out[A]
-  type State
-  type @@[A, R] = State => (A => State => Control[Out[R]]) => Control[Out[R]]
-
-  def unit[A]: (State, A) => Out[A]
-}
